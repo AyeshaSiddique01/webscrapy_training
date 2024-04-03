3d689059f08250ebfa2d42d69cedc4180e717fa5
ayesha.siddique <ayesha.siddique@arbisoft.com>
2024-04-03 14:15:04 +0500

    refactored: changes required in first PR

--- observer.py
+++ observer.py
@@ -1,19 +1,7 @@
 from abc import abstractmethod
+from random import choice
 
-
-# Topic Store class
-class TopicStore:
-    def __init__(self, topics):
-        self.topics = topics
-
-    def is_topic_available(self, topic):
-        return topic in self.topics
-
-    def remove_topic(self, topic):
-        self.topics.remove(topic)
-
-# Observable interface
-class Teacher:
+class IObservable:
     @abstractmethod
     def add(self, observer):
         pass
@@ -26,8 +14,22 @@ class Teacher:
     def notify(self, topic):
         pass
 
-# Concrete Observable class
-class ConcreteTeacher(Teacher):
+class IObserver:
+    @abstractmethod
+    def update(self, topic):
+        pass
+
+class TopicStore:
+    def __init__(self, topics):
+        self.topics = topics
+
+    def is_topic_available(self, topic):
+        return topic in self.topics
+
+    def remove_topic(self, topic):
+        self.topics.remove(topic)
+
+class ConcreteTeacher(IObservable):
     def __init__(self, topic_store):
         self.topic_store = topic_store
         self.observers = []
@@ -42,27 +44,29 @@ class ConcreteTeacher(Teacher):
         for observer in self.observers:
             observer.update(topic)
 
-    def select_topic(self, topic):
+    def remove_topic(self, topic):
         if self.topic_store.is_topic_available(topic):
             self.topic_store.remove_topic(topic)
             self.notify(topic)
 
-# Observer interface
-class Student:
-    @abstractmethod
-    def update(self, topic):
-        pass
-
-# Concrete observer class
-class ConcreteStudent(Student):
+class ConcreteStudent(IObserver):
     def __init__(self, name):
         self.name = name
+        self.selected_topic = None
+
+    def select_random_topic(self, teacher):
+        selected_topic = choice(list(teacher.topic_store.topics))
+        self.selected_topic = selected_topic
+        teacher.remove_topic(selected_topic)
+
+    def get_selected_topic(self):
+        print(f"{self.name} selected topic {self.selected_topic}")
 
     def update(self, topic):
         print(f"{self.name}: Topic {topic} has been selected.")
 
 
-no_of_topics = 20
+no_of_topics = 5
 topics = [i for i in range(1, no_of_topics + 1)]
 topic_store = TopicStore(topics)
 
@@ -73,5 +77,8 @@ students = [ConcreteStudent(f"Student{i}") for i in range(1, no_of_topics + 1)]
 for student in students:
     teacher.add(student)
 
-teacher.select_topic(4)
-print(topic_store.topics)
+for student in students:
+    student.select_random_topic(teacher)
+
+for student in students:
+    student.get_selected_topic()

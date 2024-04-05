552a427d52fdbf6cf54b044281a0d01991c07b1e
ayesha.siddique <ayesha.siddique@arbisoft.com>
2024-04-05 20:41:15 +0500

    perf: improve weighted_random_selection function

--- weighted_selection.py
+++ weighted_selection.py
@@ -3,9 +3,11 @@ import random
 def weighted_random_selection(weights):
     total_weight = sum(weights)
     probabilities = [item / total_weight for item in weights]
-    cumulative_probabilities = [sum(probabilities[:i+1]) for i in range(len(probabilities))]
+    cumulative_probability = 0
     rand = random.uniform(0, 1)
-    for index, cumulative_probability in enumerate(cumulative_probabilities):
+
+    for index, probability in enumerate(probabilities):
+        cumulative_probability += probability
         if rand < cumulative_probability:
             return weights[index]
     return None

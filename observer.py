24b7b7c858a20f63a7bcc1934d400d28e7d56e59
ayesha.siddique <ayesha.siddique@arbisoft.com>
2024-04-08 11:33:47 +0500

    feat: students selection for selecting subject

--- observer.py
+++ observer.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
-from random import choice
-
+import random
 
 class TopicStore:
     def __init__(self, topics):
@@ -42,13 +41,17 @@ class IObserver(ABC):
 class Teacher(IObservable):
     def __init__(self, topic_store):
         self.observers = []
+        self.weights = []
         self.topic_store = topic_store
 
     def add(self, observer):
         self.observers.append(observer)
+        self.weights.append(1)
 
     def remove(self, observer):
-        self.observers.remove(observer)
+        observer_index = self.observers.index(observer)
+        self.observers.remove(observer_index)
+        self.weights.remove(observer_index)
 
     def notify(self, item):
         for observer in self.observers:
@@ -58,28 +61,46 @@ class Teacher(IObservable):
         if self.topic_store.is_topic_available(topic):
             self.topic_store.remove_topic(topic)
             self.notify(topic)
+            return True
+        return False
 
-
+    def update_probability(self, observer):
+        observer_index = self.observers.index(observer)
+        self.weights[observer_index] -= self.weights[observer_index] * 0.9
+        
 class Student(IObserver):
     def __init__(self, name):
         self.name = name
-        self.selected_topic = None
+        self.selected_topics = []
         self.observable = None
 
     def register(self, observable):
         self.observable = observable
 
     def select_random_topic(self):
-        selected_topic = choice(list(self.observable.topic_store.topics))
-        self.selected_topic = selected_topic
-        self.observable.remove_topic(selected_topic)
+        selected_topic = random.choice(list(self.observable.topic_store.topics))
+        self.selected_topics.append(selected_topic)
+        if self.observable.remove_topic(selected_topic):
+            self.observable.update_probability(self)
 
     def get_selected_topic(self):
-        print(f"{self.name} selected topic {self.selected_topic}")
+        print(f"{self.name} selected topic {self.selected_topics}")
 
     def update(self, topic):
         print(f"{self.name}: Topic {topic} has been selected.")
 
+
+def weighted_random_selection(weights):
+    total_weight = sum(weights)
+    probabilities = [item / total_weight for item in weights]
+    cumulative_probability = 0
+    rand = random.uniform(0, 1)
+
+    for index, probability in enumerate(probabilities):
+        cumulative_probability += probability
+        if rand < cumulative_probability:
+            return index
+    return None
 
 no_of_topics = 5
 topics = [i for i in range(1, no_of_topics + 1)]
@@ -95,10 +116,11 @@ for student in students:
 for student in students:
     teacher.add(student)
 
-for student in students:
-    student.select_random_topic()
+while teacher.topic_store.topics:
+    selected_weight = weighted_random_selection(teacher.weights)
+    students[selected_weight].select_random_topic()
 
 for student in students:
     student.get_selected_topic()
 
-print(teacher.topic_store.topics)
+print(teacher.weights)

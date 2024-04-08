6094862753669204192861fafbb4bf95bf1d51b4
ayesha.siddique <ayesha.siddique@arbisoft.com>
2024-04-08 16:31:39 +0500

    refactor: code structure refactored

--- observer.py
+++ observer.py
@@ -31,83 +31,64 @@ class IObservable(ABC):
 class IObserver(ABC):
 
     @abstractmethod
-    def register(self, observable):
-        pass
-
-    @abstractmethod
     def update(self, item):
         pass
 
 
-class Teacher(IObservable):
-    def __init__(self, topic_store):
+class Observable(IObservable):
+    def __init__(self):
         self.observers = []
-        self.weights = []
-        self.topic_store = topic_store
 
     def add(self, observer):
         self.observers.append(observer)
-        self.weights.append(1)
 
     def remove(self, observer):
-        observer_index = self.observers.index(observer)
-        self.observers.remove(observer_index)
-        self.weights.remove(observer_index)
+        self.observers.remove(observer)
 
     def notify(self, item):
         for observer in self.observers:
             observer.update(item)
 
+
+class Teacher(Observable):
+    def __init__(self, topic_store):
+        self.topic_store = topic_store
+        super().__init__()
+
     def remove_topic(self, topic):
         if self.topic_store.is_topic_available(topic):
             self.topic_store.remove_topic(topic)
-            self.notify(topic)
+            super().notify(topic)
             return True
         return False
 
-    def reduce_weight(self, observer):
-        observer_index = self.observers.index(observer)
-        self.weights[observer_index] -= self.weights[observer_index] * 0.9
 
-
-class Student(IObserver):
+class Observer(IObserver):
     def __init__(self, name):
         self.name = name
-        self.selected_topics = []
-        self.observable = None
 
-    def register(self, observable):
-        self.observable = observable
+    def update(self, item):
+        print(f"{self.name}: {item} has been selected.")
+
+
+class Student(Observer):
+    def __init__(self, name):
+        self.selected_topics = []
+        self.teacher = None
+        super().__init__(name)
 
     def select_random_topic(self):
         selected_topic = random.choice(
-            list(self.observable.topic_store.topics))
+            list(self.teacher.topic_store.topics))
         self.selected_topics.append(selected_topic)
-        if self.observable.remove_topic(selected_topic):
-            self.observable.reduce_weight(self)
+        self.teacher.remove_topic(selected_topic)
 
     def get_selected_topic(self):
         print(f"{self.name} selected topic {self.selected_topics}")
 
-    def update(self, topic):
-        print(f"{self.name}: Topic {topic} has been selected.")
-
-
-def weighted_random_selection(weights):
-    total_weight = sum(weights)
-    probabilities = [item / total_weight for item in weights]
-    cumulative_probability = 0
-    rand = random.uniform(0, 1)
-
-    for index, probability in enumerate(probabilities):
-        cumulative_probability += probability
-        if rand < cumulative_probability:
-            return index
-    return None
-
 
 no_of_topics = 5
-topics = [i for i in range(1, no_of_topics + 1)]
+topics = [f"Topic_{i}" for i in range(1, no_of_topics + 1)]
 topic_store = TopicStore(topics)
 
 teacher = Teacher(topic_store)
@@ -115,16 +96,11 @@ teacher = Teacher(topic_store)
 students = [Student(f"Student{i}") for i in range(1, no_of_topics + 1)]
 
 for student in students:
-    student.register(teacher)
-
-for student in students:
+    student.teacher = teacher
     teacher.add(student)
 
-while teacher.topic_store.topics:
-    selected_weight = weighted_random_selection(teacher.weights)
-    students[selected_weight].select_random_topic()
+for student in students:
+    student.select_random_topic()
 
 for student in students:
     student.get_selected_topic()
-
-print(teacher.weights)

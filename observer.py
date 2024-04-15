81f8c6edf12474e4d7bdbda0fd72fae8c7aada85
ayesha.siddique <ayesha.siddique@arbisoft.com>
2024-04-15 13:59:43 +0500

    refactor: topics added in student class

--- observer.py
+++ observer.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import random
+import copy
 
 
 class IObservable(ABC):
@@ -42,16 +43,8 @@ class Observable(IObservable):
 
 
 class Teacher(Observable):
-    def __init__(self, topics):
+    def __init__(self):
         super().__init__()
-        self.topics = topics
-
-    def remove_topic(self, topic):
-        if topic in self.topics:
-            self.topics.remove(topic)
-            super().notify(topic)
-            return True
-        return False
 
 
 class Observer(IObserver):
@@ -63,26 +56,31 @@ class Observer(IObserver):
 
 
 class Student(Observer):
-    def __init__(self, name):
+    def __init__(self, name, topics):
         self.name = name
+        self.topics = copy.deepcopy(topics)
         self.selected_topics = []
         super().__init__()
 
     def select_random_topic(self):
-        selected_topic = random.choice(list(self.observable.topics))
+        selected_topic = random.choice(self.topics)
         self.selected_topics.append(selected_topic)
-        self.observable.remove_topic(selected_topic)
+        self.observable.notify(selected_topic)
 
     def get_selected_topic(self):
         print(f"{self.name} selected topic {self.selected_topics}")
+
+    def on_update(self, item):
+        super().on_update(item)
+        self.topics.remove(item)
 
 
 no_of_topics = 5
 topics = [f"Topic_{i}" for i in range(1, no_of_topics + 1)]
 
-teacher = Teacher(topics)
+teacher = Teacher()
 
-students = [Student(f"Student{i}") for i in range(1, no_of_topics + 1)]
+students = [Student(f"Student{i}", topics) for i in range(1, no_of_topics + 1)]
 
 for student in students:
     teacher.add(student)
